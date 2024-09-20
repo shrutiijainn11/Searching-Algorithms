a118cda0fa542b0cb877832d040eaa47186cdc57
Shruti Jain <158989102+shrutiijainn11@users.noreply.github.com>
2024-09-20 16:54:59 +0000

    binary search

---
+++ BinarySearch.java
@@ -0,0 +1,16 @@
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid; // Target found, return index
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search in right half
+            } else {
+                right = mid - 1; // Search in left half
+            }
+        }
+        return -1; // Target not found
+    }
+}
